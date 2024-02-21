f0fd265615cc1e712d2a3c7f0c3beaad7a925bf7
Eric Alfonso <alfonso.eric5@gmail.com>
2024-02-21 13:51:16 +0100

    Create clena architecture structure

--- app/src/main/java/com/decentrafit/decentrafit/ui/main/MainActivity.kt
+++ app/src/main/java/com/decentrafit/decentrafit/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.decentrafit.decentrafit
+package com.decentrafit.decentrafit.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.decentrafit.decentrafit.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
